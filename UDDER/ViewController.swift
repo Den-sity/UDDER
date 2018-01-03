c532c8c0b04092ac50875c4e1400607e2278e07e
ASJ <ASJ@shleeui-MacBook-Pro.local>
2018-01-03 21:19:19 +0900

    Initial Commit

---
+++ UDDER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UDDER
+//
+//  Created by ASJ on 2018. 1. 3..
+//  Copyright © 2018년 ASJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
